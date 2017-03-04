4ac161fd9f58ee27049670b66a63e9908fa9e00b
Māris Nartišs <mn@sungis.lv>
2017-03-04 20:36:23 +0200

    Create Admin page for Projects and Labels

--- src/projects/admin.py
+++ src/projects/admin.py
@@ -1,4 +1,18 @@
 # -*- coding: utf-8 -*-
 from django.contrib import admin
 
-# Register your models here.
+from projects.models import Project, Label
+
+
+class ProjectAdmin(admin.ModelAdmin):
+    list_display = ('slug', 'title', 'is_active',)
+    list_filter = ('is_active',)
+
+
+class LabelAdmin(admin.ModelAdmin):
+    list_display = ('text', 'code', 'project',)
+    list_filter = ('project__slug',)
+
+
+admin.site.register(Project, ProjectAdmin)
+admin.site.register(Label, LabelAdmin)
