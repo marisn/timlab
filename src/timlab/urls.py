3e36595fe16bc153d3fd699d939fd6a3bd153198
Māris Nartišs <mn@sungis.lv>
2017-03-18 21:16:32 +0200

    Add login and logout pages

--- src/timlab/urls.py
+++ src/timlab/urls.py
@@ -5,9 +5,12 @@ from django.conf.urls import include
 from django.conf.urls.static import static
 from django.contrib import admin
 from django.views.generic import TemplateView
+from django.contrib.auth import views as auth_views
 
 urlpatterns = [
-    url(r'^$', TemplateView.as_view(template_name='index.html')),
+    url(r'^$', TemplateView.as_view(template_name='index.html'), name='index'),
     url(r'^p/', include('projects.urls')),
     url(r'^admin/', admin.site.urls),
+    url(r'^login/$', auth_views.login, name='login'),
+    url(r'^logout/$', auth_views.logout, name='logout'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
